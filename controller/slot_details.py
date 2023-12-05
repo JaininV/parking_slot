8b6355007491260ca8c803d09e1f729615dd18cf
JaininV <jaininvekariya@gmail.com>
2023-12-05 16:28:39 -0500

    Chnages into car availibility slot

---
+++ controller/slot_details.py
@@ -0,0 +1,15 @@
+#Import users function from model folder
+from flask import Flask, render_template, jsonify, request
+from models.car_process import carParkApi, checkAvailableSlotApi
+import asyncio
+from flask import Flask, render_template
+
+def carPark():
+    data = request.form
+    page = carParkApi(data)
+    return page
+
+def checkAvailableSlot():
+    page = checkAvailableSlotApi()
+    print(page)
+    return render_template('index.html', c_data = page['code_data'], img = page['img_data'])
