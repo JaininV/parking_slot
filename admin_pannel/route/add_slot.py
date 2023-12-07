24dc21e1cef4295e1f1ff79d7b7ad15640a48dfc
JaininV <jaininvekariya@gmail.com>
2023-12-06 22:28:50 -0500

    Added alot add files

--- admin_pannel/route/add_slot.py
+++ admin_pannel/route/add_slot.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify
 from controller.slot_details import addNumber
 
-slot_details_blueprint = Blueprint('slot_details', __name__)
+add_slot_blueprint = Blueprint('add_slot', __name__)
 
 
-@slot_details_blueprint.route('/car_number', methods=['GET'])
-def add_number_api():
+@add_slot_blueprint.route('/car_number', methods=['GET'])
+def add_slot_api():
     # Your API logic here
     data = addNumber()
     return data 
